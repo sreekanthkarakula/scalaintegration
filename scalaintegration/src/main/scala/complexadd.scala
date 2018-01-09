158aecec4459c56079a5c3459cde0126f9c2de81
sreekanthkarakula <k.sreekanthreddy5a2@gmail.com>
2018-01-09 11:51:57 -0800

    complexadd with operator overloading

---
+++ scalaintegration/src/main/scala/complexadd.scala
@@ -0,0 +1,15 @@
+class Complex(val real:Int, val imaginary:Int){
+  def +(complex:Complex):Complex={
+    new Complex(real+complex.real, imaginary+complex.imaginary)
+  }
+  override def toString():String={
+    real+(if (imaginary<0) "" else "+") + imaginary + "i"
+  }
+}
+object complexadd{
+  def main(args:Array[String]){
+    val c1 = new Complex(1, 4)
+    val c2 = new Complex(2, 5)
+    println(c1+c2)
+  }
+}
